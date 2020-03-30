ac827d43342d33cab449ae8150a687289d51ef0f
Sebastian <sebastianmar00@gmail.com>
2020-03-30 15:01:04 +0200

    changes

--- app/App.svelte
+++ app/App.svelte
@@ -1,9 +1,17 @@
 <script>
     import { onMount } from "svelte"
 
-    let articles = []
-    let articlesLatest = []
-    let id = 175
+    let articlesNo = []
+    let articlesLatestNo = []
+
+    let articlesSwe = []
+    let articlesLatestSwe = []
+
+    let test 
+
+    let countryId
+
+
 
 
 
@@ -11,33 +19,59 @@
         fetch(`https://coronavirus-tracker-api.herokuapp.com/v2/locations?timelines=1`)
         .then( response => response.json() )
         .then( json => {
-            console.log("Resultater")
-            articles = json.locations
-            articlesLatest = json.locations.latest
-            console.log(articles)
+            articlesNo = [json.locations[205] + json.locations[175]]
+            articlesLatestNo = json.locations[175].latest
+            //articlesSwe = json.locations[205]
+            //articlesLatestSwe = json.locations[205].latest
+            console.log(articlesNo)
+
         }
         ).catch((err) => console.log(err))
     } )
+
+            test = [...articlesNo, ...articlesSwe]  
+
+            console.log(test)
 
 </script>
 
 <page>
     <actionBar title="Korona news" />
-    <stackLayout class="articles">
-        {#each articles as article}
-            <stackLayout>
-                <label text={article.country} />
-                <label text="Hei" />
-            </stackLayout>
-        {:else}
-            <activityIndicator busy="{true}" />
-        {/each}
-    </stackLayout>
+    <stackLayout>
+        <stackLayout>
+            
+        </stackLayout>
+    <scrollView>
+        
 
 
+        <flexboxLayout class="articles test">
+            <flexboxLayout flexDirection="column" class="">
+                <label text="Land:" />
+                <label text="Smittede:" />
+                <label text="Døde:" />
+                <label text="Friske:" />
+                <label text="Sist oppdatert:" />
+            </flexboxLayout>
+            <flexboxLayout flexDirection="column" class="data">
+                <label text=" {articlesNo.country}" />
+                <label text=" {articlesLatestNo.confirmed}" />
+                <label text=" {articlesLatestNo.deaths}" />
+                <label text=" {articlesLatestNo.recovered}" />
+                <label text=" {articlesNo.last_updated}" />
+            </flexboxLayout>
+        </flexboxLayout>
+    </scrollView>
+    
 
 </page>
 
 <style>
-
+    .articles {
+        height: 100%;
+        background-color: grey;
+    }
+    .data {
+        width: 100%
+    }
 </style>
