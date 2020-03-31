b3cafc9e33e0334fb3fb8755af5c9d88d6953f41
Sebastian <sebastianmar00@gmail.com>
2020-03-31 12:41:36 +0200

    added modul

--- app/App.svelte
+++ app/App.svelte
@@ -1,34 +1,40 @@
 <script>
     import { onMount } from "svelte"
+    import { showModal } from "svelte-native"
+    import MoreInfo from "./modals/MoreInfo.svelte"
 
+    // Array med alle landene jeg har hentet fra API
+    let europa = []
 
-    let articlesNo = []
-    let articlesLatestNo = []
-
-    let articlesSwe = []
-    let articlesLatestSwe = []
-
-    let test = []
-
-    let countryId
-
-    let Europa = [1, 3, 23, 27, 30, 92, 102, 106, 107, 122, 22, 135, 130, 137, 247, 87, 146, 149, 150, 151, 155, 159, 160, 162, 166, 175, 174, 183, 184, 186, 187, 191, 194, 197, 198, 201, "storbritania", 206, 205, 91, 213, 120, 215, 129, "vatikanstaten", 16 ]
-
-    
-
-
-
-
+    // ID til alle landene fra API som er i Europa
+    let EuropaID = [1, 3, 23, 27, 30, 92, 102, 106, 107, 122, 22, 135, 130, 137, 247, 87, 146, 149, 150, 151, 155, 159, 160, 162, 166, 175, 174, 183, 184, 186, 187, 191, 194, 197, 198, 201, "storbritania", 206, 205, 91, 213, 120, 215, 129, "vatikanstaten", 16 ]
 
     onMount( () => {
         fetch(`https://coronavirus-tracker-api.herokuapp.com/v2/locations?timelines=1`)
         .then( response => response.json() )
         .then( json => {
             // Europa
-            test = [json.locations[1],json.locations[3],json.locations[23],json.locations[27],json.locations[30],json.locations[92],json.locations[102],json.locations[106],json.locations[107],json.locations[122],json.locations[22],json.locations[135],json.locations[130],json.locations[137],json.locations[247],json.locations[87],json.locations[146],json.locations[149],json.locations[150],json.locations[151],json.locations[155],json.locations[159],json.locations[160],json.locations[162],json.locations[166],json.locations[175],json.locations[174],json.locations[183],json.locations[184], json.locations[186],json.locations[187],json.locations[191],json.locations[194],json.locations[197],json.locations[198],json.locations[201],json.locations[206],json.locations[205],json.locations[91],json.locations[213],json.locations[120],json.locations[215],json.locations[129],json.locations[16]]
+            europa = [json.locations[1],json.locations[3],json.locations[23],json.locations[27],json.locations[30],json.locations[92],json.locations[102],json.locations[106],json.locations[107],json.locations[122],json.locations[22],json.locations[135],json.locations[130],json.locations[137],json.locations[247],json.locations[87],json.locations[146],json.locations[149],json.locations[150],json.locations[151],json.locations[155],json.locations[159],json.locations[160],json.locations[162],json.locations[166],json.locations[175],json.locations[174],json.locations[183],json.locations[184], json.locations[186],json.locations[187],json.locations[191],json.locations[194],json.locations[197],json.locations[198],json.locations[201],json.locations[206],json.locations[205],json.locations[91],json.locations[213],json.locations[120],json.locations[215],json.locations[129],json.locations[16]]
         }
         ).catch((err) => console.log(err))
     } )
+
+    // Serach teksten
+    let search = ""
+    // Nytt array med filter funskjon
+    let searched = []
+    $: searched = europa.filter( europa => europa.country.toLowerCase().includes(search.toLocaleLowerCase()))
+
+    const showNew = async (searched) => {
+        await showModal(
+            {
+                page: MoreInfo,
+                props:{
+                    europa:searched
+                }
+            }
+        )
+    }
 
 </script>
 
@@ -36,32 +42,35 @@
     <actionBar on:tap={() => console.log("hei")} title="Korona news Europa" style="background-color: #222; color: white;"/>
     <stackLayout class="main" >
         <stackLayout class="searchbar">
-            <searchBar hint="Search after country"/>
+            <searchBar textFieldBackgroundColor="" bind:text={search} hint="Search after country"/>
         </stackLayout>
     <scrollView>
-    <flexboxLayout flexDirection="column" class="">
-    {#each test as test}
-    <stackLayout class="div">
-        <flexboxLayout flexDirection="column" class="table">
-            <label class="white h1 " text="{test.country}" />
-            <label class="white p" text="Smittede: {test.latest.confirmed}" />
-            <label class="white p" text="Døde: {test.latest.confirmed}" />
-            <label class="white p" text="Friske: {test.latest.recovered}" />
-        </flexboxLayout>
-    </stackLayout>
-    {:else}
-        <activityIndicator busy="{true}" />
-    {/each}
+    <flexboxLayout flexDirection="column">
+        {#each searched as item}
+            <stackLayout class="div">
+                <flexboxLayout on:tap={() => showNew(searched)} flexDirection="column" class="table">
+                    <label class="white h1 " text="{item.country}" />
+                    <label style="text-align:center;" class="white p" text="Infected: {item.latest.confirmed}" />
+                    <label style="text-align:center;" class="white p" text="Deaths: {item.latest.deaths}" />
+                    <label style="text-align:center;" class="white p" text="Healhy: {item.latest.recovered}" />
+                </flexboxLayout>
+            </stackLayout>
+        {:else}
+            <activityIndicator busy="{true}" />
+        {/each}
     </flexboxLayout>
-
     </scrollView>
     
 
 </page>
 
 <style>
+    .h1 {
+        font-size: 25;
+    }
+
     .main {
-        background-color: #777;
+        background-color: #1b1b30;
     }
 
     .white {
---
+++ app/modals/MoreInfo.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { closeModal } from "svelte-native"
+    export let europa
+</script>
+
+<frame>
+<page>
+    <scrollView>
+        <stackLayout class="container">
+            <label class="h1 white" text="{europa.country}" />
+            <label class="h1 white" text="Hei" />
+
+            <button on:tap={() => closeModal()} text="Go back" />
+        </stackLayout>
+    </scrollView>
+</page>
+</frame>
+
+<style>
+    .container {
+        background-color: #1b1b30;
+    }
+    .white {
+        color: white;
+        text-align: center;
+    }
+</style>
